2299a7d5fb42371933cdbe8662b09cde3095af06
xuefengacc <xuefeng.acc@gmail.com>
2020-03-04 11:12:43 -0500

    .
    .

---
+++ expdemo/src/main/java/expdemo/App.java
@@ -0,0 +1,13 @@
+package expdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
